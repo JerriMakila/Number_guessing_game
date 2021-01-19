beb94d522c11a608c2c7064c5f55f21e15accfe1
Jerri Mäkilä <jerri.makila@gmail.com>
2021-01-19 23:58:30 +0200

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View, Alert } from 'react-native';
 
 export default function App() {
+  const [guess, setGuess] = useState('');
+  const [guessAmount, setGuessAmount] = useState(1);
+  const [number, setNumber] = useState(Math.floor(Math .random() * 100) + 1);
+  const [message, setMessage] = useState('Guess a number between 1-100');
+
+  const makeGuess = () => {
+    if(parseInt(guess) < number){
+      setMessage('Your guess ' + guess + " is too low");
+    } else if(parseInt(guess) > number){
+      setMessage('Your guess ' + guess + " is too high");
+    } else if(parseInt(guess) === number) {
+      if(guessAmount === 1){
+        Alert.alert('You guessed the number in ' + guessAmount + " guess");
+      } else {
+        Alert.alert('You guessed the number in ' + guessAmount + " guesses");
+      }
+
+      setNumber(Math.floor(Math .random() * 100) + 1);
+      setGuessAmount(0);
+    }
+    setGuessAmount(guessAmount + 1);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.appComponent}>
+        <Text style={{fontSize:20}}>{message}</Text>
+      </View>
+      <View style={styles.appComponent}>
+        <TextInput
+        style={styles.input}
+        keyboardType='numeric'
+        value={guess}
+        onChangeText={guess => setGuess(guess)}>
+        </TextInput>
+      </View>
+      <View style={styles.appComponent}>
+        <Button onPress={makeGuess} title='MAKE GUESS'></Button>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +53,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  appComponent: {
+    marginBottom: 10,
+  },
+
+  input: {
+    borderColor: 'black',
+    borderWidth: 1,
+    fontSize: 25,
+    height: 35,
+    marginBottom: 5,
+    paddingHorizontal:5,
+    width: 50,
   },
 });
